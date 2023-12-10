9646d2d8108cab0f9c9d7ccb08d059e3ae67a105
JohannaB95 <johanna_b_g@hotmail.com>
2023-12-10 15:25:29 -0500

    Documentación del código

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
+//Se carga la libreria colors para permitir el uso de estilos de colores en la terminal
 require('colors');
 
+//Se carga el código que contiene el archivo functions_text.js en la carpeta modules_text
 const text = require('./modules_text/functions_text.js')
 
+//Se limpia la consola
 console.clear( );
 
+//Se imprime en la consola el contenido del objeto text
 console.log(text);
 
+//Se crea una interfaz con titulo, donde se visualiza el resultado de la función miNombre
 console.log(`*********************`.red)
 console.log(`*  `.red + `Nombre Completo`.bgRed + `  *`.red);
 console.log(`*  `.red + text.miNombre("jOHANNA", "bOLIVAR") + `  *`.red);
 console.log(`*********************\n`.red)
 
+//Se crea una interfaz con titulo, donde se visualiza el resultado de la función nombreMayus
 console.log(`**************************`.blue)
 console.log(`*  `.blue + `Nombre en mayúsculas`.bgBlue + `  *`.blue);
 console.log(`*        `.blue + text.nombreMayus("mariana") + `         *`.blue);
 console.log(`**************************\n`.blue)
 
+//Se crea una interfaz con titulo, donde se visualiza el resultado de la función nombreMinus
 console.log(`**************************`.cyan)
 console.log(`*  `.cyan + `Nombre en minúsculas`.bgCyan + `  *`.cyan);
 console.log(`*       `.cyan + text.nombreMinus("SEBASTIAN") + `        *`.cyan);
 console.log(`**************************\n`.cyan)
 
+//Se crea una interfaz con titulo, donde se visualiza el resultado de la función cantidadLetras
 console.log(`**************************`.yellow)
 console.log(`*   `.yellow + `Cantidad de letras`.bgYellow + `   *`.yellow);
 console.log(`*          `.yellow + text.cantidadLetras("Diego", "Velez") + `            *`.yellow);
 console.log(`**************************\n`.yellow)
 
-
+//Se declara una variable llamada nombreAprendiz, que se inicializa con un string
 let nombreAprendiz = "Santiago";
+//Se declara una variable llamada apellidoAprendiz, que se inicializa con un string
 let apellidoAprendiz = "Carvajal";
 
+/*Se declara una variable llamada nombreCompletoAprendiz1 que contendra el valor de la concatenación de 
+nombreAprendiz y apellidoAprendiz*/
 let nombreCompletoAprendiz1 = nombreAprendiz + " " + apellidoAprendiz;
 
+//Se crea una interfaz que contendra el resultado de disminuir una por una las letras de un nombre completo
 console.log(`****************************`.green)
 console.log(`*     `.green + nombreCompletoAprendiz1+ `    *`.green);
 
+//Se declara una variable llamada i que contiene la longitud de la cadena nombreCompletoAprendiz1.
 let i = nombreCompletoAprendiz1.length;
+
+//Se usa un bucle while en el que se especifica que i debe ser mayor a 0
 while(i > 0){
-   console.log(`*    `.green + i + ' ' + nombreCompletoAprendiz1.substring(0,i)+ `   *`.green);
+   /*Se imprime en la consola el número i, seguido de una subcadena de nombreCompletoAprendiz1. La 
+   subcadena se obtiene utilizando nombreCompletoAprendiz1.substring(0, i).*/
+   console.log(`*    `.green + i + ' ' + nombreCompletoAprendiz1.substring(0,i) + `   *`.green);
+   //Se reduce el valor de i en 1 en cada iteración del bucle, hasta que la condición sea falsa y se termine el bucle.
    i--;
 }
 
--- modules_text/functions_text.js
+++ modules_text/functions_text.js
@@ -1,30 +1,40 @@
+//Se declara un objeto vacío llamado text, para agrupar funciones relacionadas con funciones de texto.
 const text = { };
 
+//Se declara una función llamada miNombre que tiene dos parámetros, nombre y apellido
 function miNombre(nombre, apellido) {
+   /*Se declara una variable llamada nombreCompleto que almacenara el resultado de convertir el primer 
+   caracter de el parámetro nombre a mayúscula + el resto de nombre convertido a minúsculas + el primer 
+   caracter de apellido en mayúscula*/
    let  nombreCompleto = nombre[0].toUpperCase( ) + nombre.substring(1).toLowerCase( ) + " " + 
    apellido[0].toUpperCase( ) + apellido.substring(1).toLowerCase( )
+   //Se retornara el resultado del formateo del nombre completo
    return nombreCompleto;
 }
 
+//Se declara una función llamada nombreMayus que tiene un parámetro nombre
 function nombreMayus(nombre) {
+   //Se retornara el nombre convertido a mayúsculas
    return nombre.toUpperCase( );
 }
 
+//Se declara una función llamada nombreMinus que tiene un parámetro nombre1
 function nombreMinus(nombre1) {
+   //Se retornara el nombre convertido a minúsculas
    return nombre1.toLowerCase( );
 }
 
+//Se declara una función llamada cantidadLetras que tiene dos parámetros nombre y apellido
 function cantidadLetras(nombre, apellido){
+   //Se retornara la cantidad total sumada de caracteres de nombre y apellido
    return nombre.length +  apellido.length;
 }
 
-
-
+//Se asignan las funciones de texto como propiedades del objeto text. Esto crea métodos asociados con el objeto text.
 text.miNombre = miNombre;
 text.nombreMayus = nombreMayus;
 text.nombreMinus = nombreMinus;
 text.cantidadLetras = cantidadLetras;
 
-
-
+//Se exporta el objeto text para que pueda ser utilizado en otros archivos. Esto se logra usando module.exports.
 module.exports = text
